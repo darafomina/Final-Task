f65f6d85099b63d107e335129919d4b6ae1177f1
darafomina <darafomina13403@gmail.com>
2022-09-20 00:07:56 +0300

    Task folder added

---
+++ Task/Program.cs
@@ -0,0 +1,25 @@
+﻿void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array1 = new string[8] {"1234", "2", "hello", "world", "1567", "-2", ":-)", "Kazan"};
+string[] array2 = new string[array1.Length];
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
